b23460db56adbbd425822e2fcc4f578421e6a461
MTL <liegeheijie@gmail.com>
2019-12-15 15:34:27 +0800

    hystrix 服务降级 针对单个controller和全局配置

--- consumerDemo/src/main/java/com/mtl/consumer/controller/ConsumerController.java
+++ consumerDemo/src/main/java/com/mtl/consumer/controller/ConsumerController.java
@@ -3,12 +3,16 @@ package com.mtl.consumer.controller;
 import com.mtl.consumer.pojo.User;
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by MTL on 2019/12/12
@@ -21,7 +25,7 @@ public class ConsumerController {
     private RestTemplate restTemplate;
 
     @GetMapping("{id}")
-    @HystrixCommand(fallbackMethod = "queryByIdFallBack")
+    @HystrixCommand
     public User queryById(@PathVariable("id") Long id) {
 
         String url = "http://user-service/user/" + id;
@@ -34,5 +38,13 @@ public class ConsumerController {
         User user = new User();
         user.setUserName("未知用户错误");
         return user;
+    }
+
+    @RequestMapping("/")
+    @HystrixCommand(commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+    })
+    public List<User> get(){
+        return new ArrayList<>();
     }
 }
